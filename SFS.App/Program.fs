bb889fed3dbe874e26b189db312692dc558b5c2e
Max <me.mclifford.dev>
2020-09-07 22:13:21 +0100

    Got basic async tcp server working.

--- SFS.App/Program.fs
+++ SFS.App/Program.fs
@@ -1,8 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open SFS.Core
 
 [<EntryPoint>]
 let main argv =
+    
+    Server.start
+    
     printfn "Hello World from F#!"
     0 // return an integer exit code
--- SFS.Core/Server.fs
+++ SFS.Core/Server.fs
@@ -1,10 +1,17 @@
 namespace SFS.Core
 
+open System
+open System.Text
+open SFS.Core.Logging
+open SFS.Core.Utilities
+
 module Server =
 
     open System.Threading
     open System.Net.Sockets
 
+    let logger = Logger()
+    
     // From http://www.fssnip.net/1P/title/Easy-Wrapper-for-thread-pool-work.
     let sendToTPL task =
         Async.StartAsTask <| async { return task }
@@ -19,7 +26,34 @@ module Server =
     /// This is designed to be run off of the main thread.
     let handleConnection (connection: TcpClient) =
         // TODO add POC logging here.
-        ()
+        logger.Post { from = "Connection Handler"; message = "In handler."; time = DateTime.Now; ``type`` = Debug }
+
+        // For now accept a message, convert to string and send back a message.
+       
+        // Get the network stream
+        let stream = connection.GetStream()
+        
+        // Create a buffer to hold the message.
+        let buffer = [|for i in [0..255] -> 0uy|]
+        
+        // Read the incoming request into the buffer.
+        stream.ReadAsync(buffer, 0, 255) |> Async.AwaitTask |> Async.RunSynchronously |> ignore
+        
+        let text = Encoding.UTF8.GetString buffer 
+        
+        let msg = sprintf "Message received: '%s'" text 
+        
+        logger.Post { from = "Connection Handler"; message = text; time = DateTime.Now; ``type`` = Information }
+        
+        // Create a basic response.
+        let response = Encoding.Default.GetBytes "Hello, world!" 
+        
+        // Write a response.
+        // For now this will close the connection afterwards.
+        stream.Write(response, 0, response.Length)
+        
+        connection.Close()
+       
 
     let rec listen () =
         // Await a connection.
@@ -27,11 +61,13 @@ module Server =
         let connection = listener.AcceptTcpClient()
 
         // TODO add POC logging here.
+        logger.Post { from = "Listener"; message = "Connection received."; time = DateTime.Now; ``type`` = Debug }
 
         // Send to a background thread to handle.
         handleConnection connection |> sendToTPL |> ignore
 
         // TODO add POC logging here.
+        logger.Post { from = "Listener"; message = "Back to main."; time = DateTime.Now; ``type`` = Debug }
 
         // Once a connection accepted
         // pass it to the thread pool.
@@ -41,11 +77,13 @@ module Server =
 
     let start =
         // Start a tcp listener on specified ip and port.
+        logger.Post { from = "Main"; message = "Starting listener."; time = DateTime.Now; ``type`` = Information }
 
         listener.Start()
+        logger.Post { from = "Main"; message = "Listener started."; time = DateTime.Now; ``type`` = Success }
 
         // Pass the listener to `listen` function.
         // This will recursively handle requests.
-        listen |> ignore
+        listen() |> ignore
         
         // TODO Add shutdown.
