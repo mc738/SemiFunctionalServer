83db22e16171b72569186c969c7de24a571c5baf
Max <me.mclifford.dev>
2020-09-07 21:37:39 +0100

    Adding Logging.fs and Utilities.fs.

--- SFS.Core/Server.fs
+++ SFS.Core/Server.fs
@@ -1,5 +1,51 @@
 namespace SFS.Core
 
 module Server =
-    let test =
-        1
+
+    open System.Threading
+    open System.Net.Sockets
+
+    // From http://www.fssnip.net/1P/title/Easy-Wrapper-for-thread-pool-work.
+    let sendToTPL task =
+        Async.StartAsTask <| async { return task }
+
+    // TODO Set these in config.
+    let ip = "127.0.0.1"
+    let port = 42000
+
+    let listener = TcpListener.Create(port)
+
+    /// Handle a connection.
+    /// This is designed to be run off of the main thread.
+    let handleConnection (connection: TcpClient) =
+        // TODO add POC logging here.
+        ()
+
+    let rec listen () =
+        // Await a connection.
+        // This is blocking.
+        let connection = listener.AcceptTcpClient()
+
+        // TODO add POC logging here.
+
+        // Send to a background thread to handle.
+        handleConnection connection |> sendToTPL |> ignore
+
+        // TODO add POC logging here.
+
+        // Once a connection accepted
+        // pass it to the thread pool.
+        // The quicker this happens last issues with connections.
+
+        listen ()
+
+    let start =
+        // Start a tcp listener on specified ip and port.
+
+        listener.Start()
+
+        // Pass the listener to `listen` function.
+        // This will recursively handle requests.
+        listen |> ignore
+        
+        // TODO Add shutdown.
---
+++ SFS.Core/Utilities.fs
@@ -0,0 +1,69 @@
+namespace SFS.Core.Utilities
+
+module Threads =
+    open System.Threading.Tasks
+
+    /// Send a task to the thread pool
+    let dispatchTask task =
+        // From http://www.fssnip.net/1P/title/Easy-Wrapper-for-thread-pool-work.
+        Async.StartAsTask <| async { return task }
+
+    /// Convert a ValueTask<'a> to Async<'a>.
+    let convertValueTask<'a> (task: ValueTask<'a>) = task.AsTask() |> Async.AwaitTask
+
+
+module Messaging =
+    open System.Threading.Channels
+
+    /// A wrapper class for `System.Threading.Channels.ChannelRead`
+    type Reader<'a>(reader: ChannelReader<'a>) =
+
+        /// Set the reader listening for items.
+        member this.Start handler =
+            let rec loop () =
+                async {
+
+                    // This should get the next item async.
+                    // Thus not annoying the thread pool
+                    let! item = reader.ReadAsync() |> Threads.convertValueTask
+
+                    // Once an item is received send it to the handler function.
+                    let cont = handler item
+
+                    // Recursive loop.
+                    if cont then loop () |> ignore
+                }
+
+            loop ()
+
+    /// A wrapper class for `System.Threading.Channels.ChannelWriter`
+    type Writer<'a>(writer: ChannelWriter<'a>) =
+
+        /// Post a message to the writer.
+        member this.Post item =
+            match writer.TryWrite item with
+            | true -> Ok()
+            | false -> Error()
+
+    /// Create a mail box processor of type 'a
+    /// and assign it a handler for incoming items.
+    let createMBP<'a> handler =
+        MailboxProcessor<'a>
+            .Start(fun inbox ->
+                  let rec loop () =
+                      async {
+
+                          let! item = inbox.Receive()
+
+                          let cont = handler item
+
+                          if cont then return! loop ()
+                      }
+
+                  loop ())
+
+    let createChannel<'a> =
+        let channel = Channel.CreateUnbounded<'a>()
+        let reader = Reader channel.Reader
+        let writer = Writer channel.Writer
+        (reader, writer)
