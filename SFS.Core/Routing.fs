8dfe454e289f271c761a5e8e41649c85f507d862
Max <me.mclifford.dev>
2020-09-09 21:40:08 +0100

    Added split result.

---
+++ SFS.Core/Routing.fs
@@ -0,0 +1,68 @@
+namespace SFS.Core
+
+open System.Net.Mime
+open SFS.Core.Utilities
+
+
+module Routing =
+
+    open System.IO
+    open SFS.Core
+    open ContentTypes
+    open Http
+
+    type Route =
+        { paths: string seq
+          contentType: ContentType
+          routeType: RouteType
+          content: byte array option }
+
+    and RouteType = | Static
+    //        | Dynamic
+//
+//    and DynamicHandler =
+//        { name: string }
+//        member this.Handler(request: Request) = [|0uy|]
+
+    and RouteSetting =
+        { contentType: ContentType
+          routePaths: string seq
+          contentPath: string
+          routeType: RouteType }
+
+    and HandlerType = | Static
+
+    /// Create a static route.
+    /// Because the content is static it is
+    /// loaded into memory when the route is created.
+    let createStaticRoute settings =
+
+        match File.Exists(settings.contentPath) with
+        | true ->
+            let data = File.ReadAllBytes settings.contentPath
+            Ok
+                { paths = settings.routePaths
+                  contentType = settings.contentType
+                  routeType = settings.routeType
+                  content = Some data }
+        | false ->
+            let message =
+                sprintf "Could not load static content: '%s'." settings.contentPath
+
+            Error message
+
+    let splitResults<'a, 'b> results =
+        results
+        |> Seq.map createStaticRoute
+        |> Results.splitResults
+     
+    let createRoutes (routes: RouteSetting seq) =
+        let (successful,errors) = splitResults routes
+        
+        // TODO log any errors.        
+        successful
+        
+    let matchRoute (routes: Map<string, Route>) (notFound: Route) (route:string) =
+        match routes.TryFind route with
+        | Some route -> route 
+        | None -> notFound
--- SFS.Core/Utilities.fs
+++ SFS.Core/Utilities.fs
@@ -96,3 +96,16 @@ module Messaging =
         let reader = Reader channel.Reader
         let writer = Writer channel.Writer
         (reader, writer)
+        
+module Results =
+    
+        let splitResult<'a, 'b> (results: ('a seq * 'b seq)) (result: Result<'a, 'b>) =
+            let (success, errors) = results
+            match result with
+            | Ok value -> (Seq.append success [ value ], errors)
+            | Error value -> (success, Seq.append errors [ value ])
+        
+        /// Split a seq of results by success and failure.
+        /// Returns a tuple (successful,errors)
+        let splitResults<'a, 'b> results =
+            results |> Seq.fold splitResult (Seq.empty :> 'a seq, Seq.empty :> 'b seq)
