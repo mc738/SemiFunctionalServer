73d82933ef376186ac1ab420ac7059a34101756e
Max <me.mclifford.dev>
2020-09-10 22:51:41 +0100

    Added pipelines.

--- SFS.Core/Utilities.fs
+++ SFS.Core/Utilities.fs
@@ -116,3 +116,55 @@ module Maps =
     /// From: https://stackoverflow.com/questions/3974758/in-f-how-do-you-merge-2-collections-map-instances.
     let join (p:Map<'a,'b>) (q:Map<'a,'b>) = 
         Map(Seq.concat [ (Map.toSeq p) ; (Map.toSeq q) ])
+        
+module Pipelines =
+    
+    let bind switchFunction twoTrackInput =
+        match twoTrackInput with
+        | Ok v -> switchFunction v
+        | Error e -> Error e
+        
+    let (>==) twoTrackInput switchFunction =
+        bind switchFunction twoTrackInput
+        
+    let (>=>) switch1 switch2 x =
+        match switch1 x with
+        | Ok v -> switch2 v
+        | Error e -> Error e
+        
+    let switch f x =
+        f x |> Ok
+        
+    let map oneTrackFunction twoTrackInput =
+        match twoTrackInput with
+        | Ok v -> Ok (oneTrackFunction v)
+        | Error e -> Error e
+        
+    let tee f x =
+        f x |> ignore
+        x
+        
+    let tryCatch f x =
+        try
+            f x |> Ok
+        with
+        | ex -> Error ex.Message
+        
+    let doubleMap successFunction failureFunction twoTrackInput =
+        match twoTrackInput with
+        | Ok v -> Ok (successFunction v)
+        | Error e -> Error (failureFunction e)
+        
+    let succeed x =
+        Ok x
+    
+    let fail x =
+        Error x
+        
+        
+    let plus addSuccess addError switch1 switch2 x =
+        match (switch1 x) (switch2 x) with
+        | Ok v1, Ok v2 -> Ok (addSuccess v1 v2)
+        | Error e, Ok _ -> Error e
+        | Ok _, Error e -> Error e
+        | Error e1, Error e2 -> Error (addError e1 e2)
