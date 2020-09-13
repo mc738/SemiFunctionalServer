1f619a8752a9eaa81c32ada923e2880f4132f7e8
Max <me.mclifford.dev>
2020-09-13 20:25:23 +0100

    Finished basic WebSockets.fs and added tests.

--- SFS.Test/UnitTests/WebSockets.fs
+++ SFS.Test/UnitTests/WebSockets.fs
@@ -1,5 +1,7 @@
 namespace SFS.Test.UnitTests
 
+open System
+open System.Text
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open SFS.Core.WebSockets 
 
@@ -19,3 +21,20 @@ module WebSockets =
                 
                 Assert.AreEqual(expected, actual)
 
+        [<TestMethod>]
+        member this.``decode test message`` () =
+            let expected = "Hello, World!"
+            let rng = Random()
+            
+            let mask =  [|0uy;0uy;0uy;0uy|]
+            
+            rng.NextBytes(mask)
+            
+            let decoder = decode mask 0
+            
+            let data = Encoding.UTF8.GetBytes expected
+            
+            // Call encode 2x - it is a reversible encoding.
+            let actual = decoder data |> Array.ofSeq |> decoder |> Array.ofSeq |> Encoding.UTF8.GetString
+            
+            Assert.AreEqual(expected, actual)
